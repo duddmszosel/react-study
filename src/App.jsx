f2c2d031ac8849f82d7f9d7d2c5c1fac7a917f1e
kdc7140 <kdc7140@naver.com>
2024-03-17 20:47:21 +0900

    Router 페이지 처리

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,17 @@
+import { Routes, Route } from "react-router-dom";
+import DiaryMain from "./components/DiaryMain";
+import MusicSearch from "./components/MusicSearch";
 
-import { useEffect } from "react";
-import "./App.css";
-import SearchComponent from "./components/SearchComponent";
-import SpotifyLogin from "./components/SpotifyLogin";
 
-// secret: b175dbeb03d9439498869bdf15df94c4
 
 function App() {
-  // eslint-disable-next-line no-unused-vars
-
-  useEffect(() => {
-    const hash = window.location.hash;
-    let token = window.localStorage.getItem("token");
-
-    if (!token && hash) {
-      token = hash.substring(1).split("&").find(elem => elem.startsWith("access_token")).split("=")[1];
-      window.location.hash = "";
-      window.localStorage.setItem("token", token);
-    }
-  }, [])
-
-
   return (
-    <div className="App">
-      <SpotifyLogin />
-      <SearchComponent />
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<DiaryMain/>} />
+        <Route path="/search" element={<MusicSearch/>} />
+      </Routes>
+    </>
   );
 }
 
---
+++ src/components/MusicSearch.jsx
@@ -0,0 +1,32 @@
+
+import { useEffect } from "react";
+import "../App.css";
+import SearchComponent from "./SearchComponent";
+import SpotifyLogin from "./SpotifyLogin";
+
+// secret: b175dbeb03d9439498869bdf15df94c4
+
+const MusicSearch = () => {
+  // eslint-disable-next-line no-unused-vars
+
+  useEffect(() => {
+    const hash = window.location.hash;
+    let token = window.localStorage.getItem("token");
+
+    if (!token && hash) {
+      token = hash.substring(1).split("&").find(elem => elem.startsWith("access_token")).split("=")[1];
+      window.location.hash = "";
+      window.localStorage.setItem("token", token);
+    }
+  }, [])
+
+
+  return (
+    <div className="App">
+      <SpotifyLogin />
+      <SearchComponent />
+    </div>
+  );
+}
+
+export default MusicSearch;
